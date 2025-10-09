56a418fd460b85d84ebcd7002ea3d78a2ff3e118
enrtorgil <enrtorgil@alu.edu.gva.es>
2025-10-09 13:26:26 +0200

    fix: config js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,2 @@
 import './bootstrap';
 import '../../vendor/rappasoft/laravel-livewire-tables/resources/imports/laravel-livewire-tables-all.js';
-
-import Alpine from 'alpinejs';
-
-window.Alpine = Alpine;
-
-Alpine.start();
